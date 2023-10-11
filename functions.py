1e61127ae12efd4bc460aaf25f0026747af8607d
szaki <adamszakacsi@freemail.hu>
2023-10-11 09:55:44 +0200

    bugfix

--- functions.py
+++ functions.py
@@ -1,19 +1,20 @@
-FILEPATH = r"E:\APPS\my app\files\todos.txt"
+FILEPATH = "todos.txt"
+
 
 def get_todos(filepath=FILEPATH):
-    """" Read a text file and return the list of
+    """ Read a text file and return the list of
     to-do items.
     """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
+
 def write_todos(todos_arg, filepath=FILEPATH):
     """Write the to-do items list the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
-print(__name__)
 
 if __name__ == "__main__":
     print("Hello from functions")
@@ -25,4 +26,3 @@ Principles of productivity:
 Managing your inflow.
 Systemizing everything that repeats.
 """
-
--- web.py
+++ web.py
@@ -1,14 +1,14 @@
-import functions
 import streamlit as st
+import functions
 
 todos = functions.get_todos()
+
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
-todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
